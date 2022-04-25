54945dc9e27bf34038ba878b073d89e30c13be96
Djabrail Kurdanov <kurdanovd@gmail.com>
2022-04-25 11:06:19 +0000

    Finish caching

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,7 +1,13 @@
-from rest_framework import generics, viewsets
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_headers, vary_on_cookie
 from rest_framework.decorators import action
+
+from rest_framework import generics, viewsets
 from rest_framework.response import Response
 from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
+from rest_framework.exceptions import PermissionDenied
+
 from blog.api.serializers import (
     PostSerializer, 
     PostDetailSerializer, 
@@ -17,6 +23,10 @@ class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    @method_decorator(cache_page(300))
+    def get(self, *args, **kwargs):
+        return super(UserDetail, self).get(*args, **kwargs)
+
 
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
@@ -30,8 +40,14 @@ class TagViewSet(viewsets.ModelViewSet):
         )
 
         return Response(post_serializer.data)
+    
+    @method_decorator(cache_page(300))
+    def list(self, *args, **kwargs):
+        return super(TagViewSet, self).list(*args, **kwargs)
 
-
+    @method_decorator(cache_page(300))
+    def retrieve(self, *args, **kwargs):
+        return super(TagViewSet, self).retrieve(*args, **kwargs)
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -42,3 +58,18 @@ class PostViewSet(viewsets.ModelViewSet):
         if self.action in ("list", "create"):
             return PostSerializer
         return PostDetailSerializer
+
+    @method_decorator(cache_page(300))
+    @method_decorator(vary_on_headers('Authorization'))
+    @method_decorator(vary_on_cookie)
+    @action(detail=False, name="Posts by the logged in user")
+    def mine(self, request):
+        if request.user.is_anonymous:
+            raise PermissionDenied("You most be logged in to see which Posts are yours")
+        posts = self.get_queryset().filter(author=request.user)
+        serializer = PostSerializer(posts, many=True, context={"request": request})
+        return Response(serializer.data)
+
+    @method_decorator(cache_page(120))
+    def list(self, *args, **kwargs):
+        return super(PostViewSet, self).list(*args, **kwargs)
